f28ca8faa3f64e7dac917f599d6f4127b860895b
olix3001 <olimioli9@gmail.com>
2023-05-26 23:38:39 +0200

    feat: run basic rust code

--- src/runtimes/mod.rs
+++ src/runtimes/mod.rs
@@ -1,30 +1,27 @@
 use crate::compilers::{CompiledCode, Compiler};
+use std::{fmt::Debug};
 
 pub mod wasm_runtime;
 
 /// Trait for every code runtime.
 /// Represents a runtime that can be used to run some code.
 pub trait CodeRuntime: Send + Sync + Sized {
+    /// Configuration for the runtime.
+    type Config: Send + Sync + Sized + Debug + Clone + Default;
+    /// Error type for the runtime.
+    type Error: Send + Sync + Sized + 'static;
+    
     /// Run compiled code. Returns saved output (if any) and exit code.
-    fn run<C: Compiler<Self>>(code: CompiledCode<C, Self>) -> ExecutionResult;
-}
-
-/// Configuration for runtimes.
-#[derive(Debug, Clone)]
-pub struct RuntimeConfig {
-    /// Maximum time (in seconds) that the code can run.
-    pub max_time: u32,
-    /// Maximum memory (in bytes) that the code can use.
-    pub max_memory: u32,
-    /// Whether to save the output of the code.
-    pub save_output: bool,
+    fn run<C: Compiler<Self>>(code: &CompiledCode<C, Self>, config: Self::Config) -> Result<ExecutionResult, Self::Error>;
 }
 
 /// Result of running code.
 #[derive(Debug, Clone)]
 pub struct ExecutionResult {
     /// Output of the code (if any).
-    pub output: Option<String>,
+    pub stdout: Option<String>,
+    /// Error of the code (if any).
+    pub stderr: Option<String>, 
     /// Exit code of the code.
     pub exit_code: i32,
 }
--- src/runtimes/wasm_runtime.rs
+++ src/runtimes/wasm_runtime.rs
@@ -1,13 +1,166 @@
+use std::{io::{Read, BufReader}, path::PathBuf, fs::File, str::{from_utf8_unchecked, from_utf8}};
+
 use crate::compilers::{Compiler, CompiledCode};
 
 use super::{CodeRuntime, ExecutionResult};
+use wasmtime_wasi::WasiCtxBuilder;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Default)]
 pub struct WasmRuntime;
+
+/// Configuration for wasm runtime.
+#[derive(Debug, Clone)]
+pub struct WasmConfig {
+    /// Maximum memory size in bytes.
+    /// Default: 256 MiB
+    pub max_memory_size: usize,
+    /// Maximum runtime in seconds. <br/>
+    /// **Note:** This is not implemented yet.
+    pub max_runtime: usize,
+
+    /// File containing stdin to be used by the code.
+    pub stdin: InputData,
+
+    /// Custom wasm config.
+    pub custom_config: wasmtime::Config
+}
+
+/// Represents input data for the code.
+#[derive(Debug, Clone)]
+pub enum InputData {
+    /// Stdin will be read from the given file.
+    File(PathBuf),
+    /// Stdin will be read from the given string.
+    String(String),
+    /// Stdin will be ignored.
+    Ignore
+}
+
+impl Default for WasmConfig {
+    fn default() -> Self {
+        Self {
+            max_memory_size: 256 * 1024 * 1024,
+            max_runtime: 0,
+            stdin: InputData::Ignore,
+            custom_config: wasmtime::Config::default()
+        }
+    }
+}
 
 /// Runtime for wasm code.
 impl CodeRuntime for WasmRuntime {
-    fn run<C: Compiler<Self>>(code: CompiledCode<C, Self>) -> ExecutionResult {
-        todo!()
+    /// Configuration for the runtime.
+    type Config = WasmConfig;
+    /// Error type for the runtime.
+    type Error = wasmtime::Error;
+
+    /// Uses `wasmtime` to run the code.
+    fn run<C: Compiler<Self>>(code: &CompiledCode<C, Self>, config: Self::Config) -> Result<ExecutionResult, Self::Error> {
+        // Create config for wasmtime.
+        let mut wasm_config = config.custom_config;
+
+        // Set maximum memory size.
+        wasm_config.static_memory_maximum_size(config.max_memory_size as u64);
+        wasm_config.static_memory_forced(true);
+
+        // Create wasi pipes.
+        let stdout = wasi_common::pipe::WritePipe::new_in_memory();
+        let stderr = wasi_common::pipe::WritePipe::new_in_memory();
+
+        // Ensure everything is dropped before we try to read from the pipes.
+        {
+            // Create wasmtime engine.
+            let engine = wasmtime::Engine::new(&wasm_config)?;
+
+            // Read module from file.
+            let module = wasmtime::Module::from_file(&engine, &code.executable.as_ref().unwrap())?;
+
+            // Create wasmtime linker.
+            let mut linker = wasmtime::Linker::new(&engine);
+            wasmtime_wasi::add_to_linker(&mut linker, |s| s)?;
+
+            // Create wasi context.
+            let mut wasi = WasiCtxBuilder::new()
+                .stdout(Box::new(stdout.clone()))
+                .stderr(Box::new(stderr.clone()));
+
+            // And more pipes.
+            match config.stdin {
+                InputData::File(path) => {
+                    let file = File::open(path)?;
+                    let reader = BufReader::new(file);
+                    let stdin = wasi_common::pipe::ReadPipe::new(reader);
+                    wasi = wasi.stdin(Box::new(stdin));
+                },
+                InputData::String(string) => {
+                    let stdin = wasi_common::pipe::ReadPipe::from(string);
+                    wasi = wasi.stdin(Box::new(stdin));
+                },
+                InputData::Ignore => {}
+            }
+
+            // Build wasi context.
+            let wasi = wasi.build();
+
+            // Create wasmtime store.
+            let mut store = wasmtime::Store::new(&engine, wasi);
+
+            // Link module.
+            linker.module(&mut store, "", &module)?;
+
+            // Get and run main function.
+            linker
+                .get_default(&mut store, "")?
+                .typed::<(), ()>(&store)?
+                .call(&mut store, ())?;
+
+            // Explicitly drop store.
+            drop(store);
+        }
+
+        // Parse stdout and stderr into strings.
+        let stdout = match stdout.try_into_inner() {
+            Ok(stdout) => {
+                // Read stdout into string.
+                from_utf8(stdout.into_inner().as_slice()).map(|s| s.to_owned()).ok()
+            },
+            Err(_) => None
+        };
+
+        let stderr = match stderr.try_into_inner() {
+            Ok(stderr) => {
+                // Read stderr into string.
+                from_utf8(stderr.into_inner().as_slice()).map(|s| s.to_owned()).ok()
+            },
+            Err(_) => None
+        };
+
+        // Return execution result.
+        Ok(ExecutionResult {
+            stdout,
+            stderr,
+            exit_code: 0,
+        })
     }
 }   
+
+#[cfg(test)]
+mod tests {
+    use crate::compilers::rust::RustCompiler;
+
+    use super::*;
+
+    #[test]
+    fn test_wasm_runtime() {
+        let code = r#"
+            fn main() {
+                println!("Hello, world!");
+            }
+        "#;
+
+        let compiled_code = RustCompiler.compile(&mut code.as_bytes(), Default::default()).unwrap();
+        let result = WasmRuntime::run(&compiled_code, Default::default()).unwrap();
+    
+        assert_eq!(result.stdout, Some("Hello, world!\n".to_owned()));
+    }
+}
